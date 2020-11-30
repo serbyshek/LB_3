45b1dbb0795df9335070aebec0ba7de4d68f3c5c
serbyshek <66415549+serbyshek@users.noreply.github.com>
2020-11-30 12:19:08 +0300

    Add files via upload

--- individual1.py
+++ individual1.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
 n = int(input("Сколько вам лет? "))
 if n < 100:
     print("Вы ввели некорректное число!")
-elif n % 10 == 1:
+elif n % 10 == 1 and n % 100 != 11:
     print("Мне", n, "год")
-elif n % 10 == 0 or n % 10 <= 5:
+elif n % 10 > 0 and n % 10 <= 5 and n % 100 > 20:
     print("Мне", n, "года")
 else:
     print("Мне", n, "лет")
