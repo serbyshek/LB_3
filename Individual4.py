3e870ca208c95a57ca6e9f9654bf4ca77c85cbbc
serbyshek <66415549+serbyshek@users.noreply.github.com>
2020-12-01 17:15:59 +0300

    Add files via upload

--- Individual4.py
+++ Individual4.py
@@ -2,12 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-
 import math
 import sys
-
 EPS = 1e-10
-
 if __name__ == '__main__':
     x = float(input("Введите x "))
     if x == 0:
@@ -19,5 +16,4 @@ if __name__ == '__main__':
         a *= (-x * (2 * k + 1) ** 2)/(4 * (k + 1) ** 2)
         S += a
         k += 1
-# Вывести значение функции.
-    print(f"Si({x}) = {S}")
+print(f"Si({x}) = {S}")
